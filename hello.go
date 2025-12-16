cf93b93d153cdb7052006b31cc7dad6b957249a0
XTLOL1 <xtlol99@gmail.com>
2025-12-16 17:13:09 +0000

    Added a map to simplify language checking and addition

--- hello.go
+++ hello.go
@@ -1,10 +1,25 @@
 package main
 import "fmt"
 
-func Hello(name string) string{
-	return "Hello, " + name
+func Hello(name string, language string) string{
+	languageMap := map[string]string{
+		"English": "Hello, ",
+		"Portuguese": "Ola, ",
+	}
+	if name == "" {
+		name = "World"
+	}
+	if language == "" {
+		language = "English"
+	}
+	val, exists := languageMap[language]
+	if exists {
+		return val + name
+	} else {
+		return "Language Unknown"
+	}
 }
 
 func main(){
-	fmt.Println(Hello("smth"))
+	fmt.Println(Hello("", ""))
 }
