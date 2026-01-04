1cc1ca86af899fa8e2c5964aa316ef20e2194727
XTLOL1 <xtlol99@gmail.com>
2026-01-04 16:29:00 +0000

    Dependency Injection

---
+++ depinj/greet.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
+
+//pretty much dependency injection is building modular code 
+//with interfaces so that it can be used for various objectives
